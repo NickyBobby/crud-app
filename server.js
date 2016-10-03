9ba8846d762a36ea566cfec64c75730a5e96094e
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-10-03 16:47:22 -0600

    Adds start to UPDATE functionality

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser')
 const app = express();
 const MongoClient = require('mongodb').MongoClient
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
 var db;
@@ -29,5 +30,21 @@ MongoClient.connect('mongodb://nickynonaps:Gold420420@ds049456.mlab.com:49456/st
 
       console.log('saved to database')
       res.redirect('/')
+    })
+  })
+
+  app.put('/quotes', (req, res) => {
+    db.collection('quotes')
+    .findOneAndUpdate({name: 'Yoda'}, {
+      $set: {
+        name: req.body.name,
+        quote: req.body.quote
+      }
+    }, {
+      sort: {_id: -1},
+      upsert: true
+    }, (err, result) => {
+      if (err) return res.send(err)
+      res.send(result)
     })
   })
