d87555741d16ff9a1d1a8526e17edf7ea6757810
Nicholas Dorans <Nicky_Bobby@icloud.com>
2016-10-03 16:28:21 -0600

    Adds CREATE and READ operations for CRUD app in express

--- server.js
+++ server.js
@@ -1,1 +1,33 @@
-console.log('May the Node be with you')
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const MongoClient = require('mongodb').MongoClient
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.set('view engine', 'ejs')
+
+var db;
+
+MongoClient.connect('mongodb://nickynonaps:Gold420420@ds049456.mlab.com:49456/star-wars-quotes', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+  app.get('/', (req, res) => {
+    db.collection('quotes').find().toArray((err, result) => {
+      if (err) return console.log(err)
+      res.render('index.ejs', {quotes: result})
+    })
+  })
+
+  app.post('/quotes', (req, res) => {
+    db.collection('quotes').save(req.body, (err, result) => {
+      if (err) return console.log(err)
+
+      console.log('saved to database')
+      res.redirect('/')
+    })
+  })
